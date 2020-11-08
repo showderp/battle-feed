78713cff539f78de5efb0e1e54525f60e75d4d8d
Mr. Sableye <mister.sableye@gmail.com>
2020-11-07 21:19:49 -0800

    Adds missing .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-typescript',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
